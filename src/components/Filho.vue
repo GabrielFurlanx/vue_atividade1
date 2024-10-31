437e625ad47594b272ead76a0432f1952c1deead
Gabriel F <105085075+GabrielFurlanx@users.noreply.github.com>
2024-10-31 19:06:12 -0300

    Add files via upload

---
+++ src/components/Filho.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div>
+      <p>Nome: <input type="text" v-model="novoFilho.nome" placeholder="Digite o nome" /></p>
+    </div>
+    <div>
+      <p>Idade: <input type="number" v-model="novoFilho.idade" placeholder="Digite a idade" /></p>
+    </div>
+    <div>
+      <p>Gênero:</p>
+      <label>
+        <input type="radio" v-model="novoFilho.genero" value="masculino" /> Masculino
+      </label>
+      <label>
+        <input type="radio" v-model="novoFilho.genero" value="feminino" /> Feminino
+      </label>
+    </div>
+    <div>
+      <p>Inteligência: <input type="text" v-model="novoFilho.inteligencia" placeholder="Digite a inteligência" /></p>
+    </div>
+
+    <button @click="criarFilho">Criar Filho</button>
+    <button @click="excluirFilho(index)">Excluir Filho</button>
+
+    <hr />
+
+    <div v-if="filhos.length">
+      <h2>Lista de Filhos Criados</h2>
+      <div v-for="(filho, index) in filhos" :key="index">
+        <p><strong>Filho {{ index + 1 }}:</strong></p>
+        <p>Nome: {{ filho.nome }}</p>
+        <p>Idade: {{ filho.idade }}</p>
+        <p>Gênero: {{ filho.genero }}</p>
+        <p>Inteligência: {{ filho.inteligencia }}</p>
+        <hr />
+      </div>
+    </div>
+    <div v-else>
+      <p>A lista está vazia.</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const novoFilho = ref({
+  nome: "",
+  idade: null,
+  genero: "",
+  inteligencia: ""
+})
+
+const filhos = ref([])
+
+const criarFilho = () => {
+  filhos.value.push({ ...novoFilho.value })
+  novoFilho.value.nome = ""
+  novoFilho.value.idade = null
+  novoFilho.value.genero = ""
+  novoFilho.value.inteligencia = ""
+}
+
+const excluirFilho = (index) => {
+  filhos.value.splice(index, 1)
+}
+</script>
---
+++ src/components/MeuComponente.vue
@@ -0,0 +1,31 @@
+<template>
+  <p> {{ valor }}</p>
+  <button style="display: block" @click="incrementar"> Incrementar </button>
+</template>
+
+<script setup>
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue'
+
+  const valor = ref(0)
+
+  function incrementar() {
+    valor.value++
+  }
+
+  onBeforeMount(() => {
+    console.log('Antes de montar')
+  })
+
+  onMounted(() => {
+    console.log('Componente Montado')
+  })
+
+  onBeforeUpdate(() => {
+    console.log('Antes de atualizar')
+  })
+
+  onUpdated(() => {
+    console.log('Depois de Atualizar')
+  })
+
+</script>
